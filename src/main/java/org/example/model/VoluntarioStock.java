7aa611d2b0d55d9e4aacffc138149b670b04f786
trofense4785 <1241095@isep.ipp.pt>
2025-05-13 22:17:03 +0100

    classe voluntariostock

---
+++ src/main/java/org/example/model/VoluntarioStock.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public class VoluntarioStock extends Voluntario {
+    public VoluntarioStock(String nome, int numeroAluno, String curso, Instituicao instituicao) {
+        super(nome, numeroAluno, curso, instituicao);
+    }
+}
