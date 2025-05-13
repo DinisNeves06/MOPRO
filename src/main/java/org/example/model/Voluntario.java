9020487d835cc4d9ae36e92224ee024058eaa708
trofense4785 <1241095@isep.ipp.pt>
2025-05-13 22:16:53 +0100

    classe voluntario

--- src/main/java/org/example/model/Voluntario.java
+++ src/main/java/org/example/model/Voluntario.java
@@ -1,76 +1,28 @@
 package org.example.model;
 
-public class Voluntario {
-    private String nome;
-    private int numeroAluno;
-    private Instituição instituicao;
-    private Barraca barracaAssociada;
+public abstract class Voluntario {
+    protected final String nome;
+    protected final int numeroAluno;
+    protected final String curso;
+    protected final Instituicao instituicao;
 
-    private static final String NOME_POR_OMISSAO = "sem nome";
-    private static final int NUMERO_ALUNO_POR_OMISSAO = 0;
-    private static final Instituição INSTITUIÇÃO_POR_OMISSAO = new Instituição("");
-
-    public Voluntario (String nome, int numeroAluno, Instituição instituição){
+    public Voluntario(String nome, int numeroAluno, String curso, Instituicao instituicao) {
         this.nome = nome;
         this.numeroAluno = numeroAluno;
-        this.instituicao = instituição;
-        this.barracaAssociada = null;
-    }
-
-    public Voluntario(){
-        this.nome = NOME_POR_OMISSAO;
-        this.numeroAluno = NUMERO_ALUNO_POR_OMISSAO;
-        this.instituicao = INSTITUIÇÃO_POR_OMISSAO;
-        this.barracaAssociada = null;
-    }
-
-    public Voluntario(Voluntario v){
-        this.nome = v.nome;
-        this.numeroAluno = v.numeroAluno;
-        this.instituicao = v.instituicao;
-        this.barracaAssociada = v.barracaAssociada;
-
+        this.curso = curso;
+        this.instituicao = instituicao;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getNumeroAluno() {
-        return numeroAluno;
-    }
-
-    public void setNumeroAluno(int numeroAluno) {
-        this.numeroAluno = numeroAluno;
-    }
-
-    public Instituição getInstituicao() {
+    public Instituicao getInstituicao() {
         return instituicao;
-    }
-
-    public void setInstituicao(Instituição instituicao) {
-        this.instituicao = instituicao;
-    }
-
-    public Barraca getBarracaAssociada() {
-        return barracaAssociada;
-    }
-
-    public void setBarracaAssociada(Barraca barracaAssociada) {
-        this.barracaAssociada = barracaAssociada;
     }
 
     @Override
     public String toString() {
-        return "Voluntario{" +
-                "nome='" + nome + '\'' +
-                ", numeroAluno=" + numeroAluno +
-                ", instituicao=" + instituicao +
-                ", barracaAssociada=" + barracaAssociada +
-                '}';
+        return nome + " (" + numeroAluno + ") - " + curso;
     }
 }
