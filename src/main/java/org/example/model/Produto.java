7f8c0350f79abe9f9db610af0ed197af8fd13298
trofense4785 <1241095@isep.ipp.pt>
2025-05-13 22:16:43 +0100

    classe produto

--- src/main/java/org/example/model/Produto.java
+++ src/main/java/org/example/model/Produto.java
@@ -1,64 +1,36 @@
 package org.example.model;
 
-import java.util.Objects;
-
 public class Produto {
-    private String nome;
-    private double preco;
+    private final String nome;
+    private int stock;
 
-    private static final String NOME_POR_OMISSAO = "por definir";
-    private static final double PRECO_POR_OMISSAO = 0.0;
-
-    public Produto(String nome, double preco) {
+    public Produto(String nome, int stock) {
         this.nome = nome;
-        this.preco = preco;
-    }
-
-    public Produto() {
-        this.nome = NOME_POR_OMISSAO;
-        this.preco = PRECO_POR_OMISSAO;
-    }
-
-    public Produto(Produto produto) {
-        this.nome = produto.nome;
-        this.preco = produto.preco;
+        this.stock = stock;
     }
 
     public String getNome() {
-
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public int getStock() {
+        return stock;
     }
 
-    public double getPreco() {
-        return preco;
+    public void adicionar(int quantidade) {
+        this.stock += quantidade;
     }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Produto produto = (Produto) o;
-        return Double.compare(preco, produto.preco) == 0 && Objects.equals(nome, produto.nome);
+    public boolean vender(int quantidade) {
+        if (stock >= quantidade) {
+            stock -= quantidade;
+            return true;
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("");
-        sb.append("\nNome: ").append(nome);
-        sb.append("\nPreco: ").append(preco).append("€");
-        sb.append("");
-        return sb.toString();
+        return nome + ": " + stock;
     }
-
 }
