f419888d51ff6409f7fac65dbfe593f254700c0e
trofense4785 <1241095@isep.ipp.pt>
2025-05-13 22:15:15 +0100

    classe instituicao

---
+++ src/main/java/org/example/model/Instituicao.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+public class Instituicao {
+    private final String nome;
+
+    public Instituicao(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
