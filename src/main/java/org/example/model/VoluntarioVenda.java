4220d9eda23f93b8800857942f7e335eb4c746c1
trofense4785 <1241095@isep.ipp.pt>
2025-05-13 22:17:12 +0100

    classe voluntariovenda

---
+++ src/main/java/org/example/model/VoluntarioVenda.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public class VoluntarioVenda extends Voluntario {
+    public VoluntarioVenda(String nome, int numeroAluno, String curso, Instituicao instituicao) {
+        super(nome, numeroAluno, curso, instituicao);
+    }
+}
